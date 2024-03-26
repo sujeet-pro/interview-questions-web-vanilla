fed737eadcba1bc346c7e1ece65a337eeadb238a
Sujeet <contact@sujeet.pro>
2024-03-26 23:39:58 +0530

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,24 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import fs from "node:fs";
 
-const ignoredFolders = new Set(["dist", "node_modules", "public"]);
-const files = fs.readdirSync(__dirname);
-const folders = files.filter(
-  (file) => fs.statSync(file).isDirectory() && !ignoredFolders.has(file)
-);
-
-console.log(`folder: ${folders.join(",")}`);
-const inputs = folders
-  .map((folderName) => [
-    folderName,
-    resolve(__dirname, `${folderName}/index.html`),
+const inputs = fs
+  .readdirSync(resolve(__dirname, "src"))
+  .map((fileOrFolderName) => [
+    fileOrFolderName,
+    resolve(__dirname, `src/${fileOrFolderName}/index.html`),
   ])
   .filter(([k, v]) => fs.existsSync(v));
 
-console.log(inputs.map(([k, v]) => v).join(" - "));
 export default defineConfig({
+  base: "/ui-challenges-vanilla/",
+  root: "./src", // Setting the root to be src
+
   build: {
+    outDir: "../dist", // Relative to project root
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "src/index.html"),
         ...Object.fromEntries(inputs),
       },
     },
